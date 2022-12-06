7889713a6d6e1c15fc6edc39813e641ce761052f
Sahil Shendurkar <64739704+Sahil0402@users.noreply.github.com>
2022-12-06 14:16:43 +0530

    Changed the logo

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,37 @@
-import{ useAddress, useMetamask }from '@thirdweb-dev/react'
+import { useAddress, useMetamask } from '@thirdweb-dev/react'
 import Main from '../components/Home'
+import Image from 'next/image'
+import banner from '../assets/banner.png'
 
-const style= {
-  wrapper:'flex h-screen items-center justify-center',
-  connectWalletButton:'rounded-lg border border-black px-10 py-5 transition-all hover:bg-black hover:text-white',
+const img = require("../assets/banner.png");
+const style = {
+  wrapper: 'flex h-screen items-center justify-center',
+  connectWalletButton: 'rounded-lg border border-black px-10 py-5 transition-all hover:bg-black hover:text-blue-500',
+}
+const styling = {
+  backgroundImage: `url('${img}')`,
+  width:"100%",
+  height:"100%",
 }
 
 export default function Home() {
-  
-  const connectWithMetamask=useMetamask()
-  const address=useAddress()
-  
+
+  const connectWithMetamask = useMetamask()
+  const address = useAddress()
+
   console.log(address)
 
   const Auth = () => {
-    return(
-     <div className={style.wrapper}>
-  <button 
-    onClick={connectWithMetamask}
-    className={style.connectWalletButton}
-    >Conntect Metamask
-  </button>
-    </div>
+    return (
+      <div className={style.wrapper}>
+        <button
+          onClick={connectWithMetamask}
+          className={style.connectWalletButton}
+        >Connect Metamask
+        </button>
+      </div>
     )
   }
-  
-  return <>{address ? <Main/> : Auth()}</>
-} 
- 
+
+  return <>{address ? <Main /> : Auth()}</>
+}
